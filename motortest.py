b1797e554331651f2993fff657f9c3d79a40f988
Ray Lennon <raylennon27@gmail.com>
2023-03-28 16:09:18 -0400

    idk testing

--- motortest.py
+++ motortest.py
@@ -8,8 +8,8 @@ address = 0x60
 # Initialize I2C (SMBus)
 bus = smbus.SMBus(channel)
 
-# Create a sawtooth wave 16 times
-for i in range(16):
+print("writing...")
+for i in range(0x40,0x40+50):
 
     bus.write_byte_data(address, i, 0)
     bus.write_byte_data(address, i+1, 0xFF)
