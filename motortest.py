30168a04cb24fb76791491812df9491060735f12
Ray Lennon <raylennon27@gmail.com>
2023-03-28 16:05:34 -0400

    idk testing

--- motortest.py
+++ motortest.py
@@ -1,4 +1,5 @@
 import smbus
+import time
 channel = 1
 
 #  MCP4725 defaults to address 0x60
@@ -10,4 +11,6 @@ bus = smbus.SMBus(channel)
 # Create a sawtooth wave 16 times
 for i in range(16):
 
-    bus.write_i2c_block_data(address, i, 0)
+    bus.write_byte_data(address, i, 0)
+    bus.write_byte_data(address, i+1, 0xFF)
+    time.sleep(1000)
