c0ddbdf84f346332f75182409025779e6cf8fba9
raylennon <raylennon27@gmail.com>
2023-04-09 17:14:49 -0400

    idk

--- ROBOT.py
+++ ROBOT.py
@@ -65,8 +65,8 @@ def gen(camera):
 def command(cmd=None):
     r = cmd.lower()
     if cmd=="forward":
-        motor3.throttle = 0.7
-        motor4.throttle = -1
+        motor3.throttle = 1
+        motor4.throttle = -0.7
     elif cmd=="stop":
         motor3.throttle = motor4.throttle = 0
     elif cmd=="back":
