793e7465e21171f59f545950ce6067f532a82810
Ray Lennon <raylennon27@gmail.com>
2023-04-09 20:41:07 -0400

    timeout feature

--- ROBOT.py
+++ ROBOT.py
@@ -95,11 +95,18 @@ def command(cmd=None):
     last_command_time = time.time() # update the last command time
     return r
 
+dontcheck = False
+
 def check_for_timeout():
     global last_command_time
+    global dontcheck
+
     while True:
-        if time.time() - last_command_time > 2: # check if the last command was more than 2 seconds ago
+        if (not dontcheck) and time.time() - last_command_time > 2: # check if the last command was more than 2 seconds ago
             motor3.throttle = motor4.throttle = 0
+            dontcheck = True
+        else:
+            dontcheck = False
 
 timeout_thread = threading.Thread(target=check_for_timeout)
 timeout_thread.start()
