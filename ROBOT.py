829c96bbb7d5f83b0f9cb2dbe0b7e2dece07dd7f
Ray Lennon <raylennon27@gmail.com>
2023-04-10 13:55:03 -0400

    timeout feature improved w/ heartbeat

--- ROBOT.py
+++ ROBOT.py
@@ -104,7 +104,7 @@ res = 30
 @app.route('/still_alive')
 def imokay():
     last_command_time = time.time()
-    return 0
+    return "okay"
 
 def check_for_timeout():
     global last_command_time
