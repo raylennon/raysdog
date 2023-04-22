46294e2699d96fe3e13585bc178536e27a077a45
raylennon <raylennon27@gmail.com>
2023-04-21 21:38:45 -0400

    overhaul

---
+++ static/js/commands.js
@@ -0,0 +1,54 @@
+const buttons = document.querySelectorAll("button");
+const pressedKeys = { left: false, up: false, right: false, down: false };
+
+function handlePress(id, down) {
+  const button = document.querySelector(`#${id}`);
+  if (pressedKeys[id] !== down) {
+    pressedKeys[id] = down;
+    directionUpdate(id, down);
+    button.style.backgroundColor = down ? "darkgrey" : "";
+  }
+}
+
+buttons.forEach((button) => {
+  button.addEventListener("mousedown", () => handlePress(button.id, true));
+  button.addEventListener("mouseup", () => handlePress(button.id, false));
+});
+
+document.addEventListener("keydown", (event) => {
+  const id = { "ArrowLeft": "left", "ArrowUp": "up", "ArrowRight": "right", "ArrowDown": "down" }[event.key];
+  if (id && !pressedKeys[id]) {
+    handlePress(id, true);
+  }
+});
+
+document.addEventListener("keyup", (event) => {
+  const id = { "ArrowLeft": "left", "ArrowUp": "up", "ArrowRight": "right", "ArrowDown": "down" }[event.key];
+  if (id && pressedKeys[id]) {
+    handlePress(id, false);
+  }
+});
+
+function directionUpdate(id, down) {
+  // console.log(`Button ${id} was ${down ? "pressed" : "released"}`);
+  // console.log(pressedKeys);
+  let dir = ""; const baseUrl=dir;
+  if (pressedKeys.left) {dir += "L";}
+  if (pressedKeys.right) {dir += "R";}
+  if (pressedKeys.up) {dir += "U";}
+  if (pressedKeys.down) {dir += "D";}
+  if (dir === baseUrl) {dir += "STOP";}
+
+  const data = {
+    command: 'dir',
+    direction: dir
+  };
+
+  fetch("/webhook", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  });
+}
