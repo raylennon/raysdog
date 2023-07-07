7458c44c208604d34087687cfc8d3870c6f6308c
Ray Lennon <raylennon27@gmail.com>
2023-07-06 23:35:43 -0400

    backlight attempt

--- WEB_ROBOT.py
+++ WEB_ROBOT.py
@@ -74,11 +74,11 @@ def on_server_message(new_status):
 
     if not new_status['message'] == status['message']:
         print("MESSAGE UPDATE")
-        lcd.clear()
+        # lcd.clear()
         text = new_status['message']
         if len(text) > 16:
-            t1 = text[:16]
-            t2 = text[16:]
+            t1 = text[:16] + ' '*max(0,(16-len(text)))
+            t2 = text[16:]+ ' '*max(0,(32-len(text)))
             lcd.text(t1, 1)
             lcd.text(t2, 2)
         else:
