6ee43d43dae395973ce60d2f33100edadc1edee4
raylennon <raylennon27@gmail.com>
2023-08-25 13:39:21 -0400

    undo flip

--- oldversion/ROBOT.py
+++ oldversion/ROBOT.py
@@ -87,7 +87,7 @@ import cv2
 def gen(camera):
     while True:
         frame = camera.get_frame()
-        frame = cv2.flip(frame, 0)
+        # frame = cv2.flip(frame, 0)
         yield (b'--frame\r\n'
                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
 
