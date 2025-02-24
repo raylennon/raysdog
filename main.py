5039418b0c08e63abe1f3845a1c1872139de5bfd
Ray Lennon <48305386+raylennon@users.noreply.github.com>
2025-02-24 12:04:01 -0500

    Resolution back down

--- main.py
+++ main.py
@@ -77,7 +77,7 @@ class StreamingServer(socketserver.ThreadingMixIn, server.HTTPServer):
 
 
 picam2 = Picamera2()
-picam2.configure(picam2.create_video_configuration(main={"size": (2028,1080)}))
+picam2.configure(picam2.create_video_configuration(main={"size": (640, 480)}))
 output = StreamingOutput()
 picam2.start_recording(JpegEncoder(), FileOutput(output))
 
